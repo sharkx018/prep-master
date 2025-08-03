c82f45d7201255693b16cbdcc02ef1b958117a6c
mukul_verma <mukul.verma@gokwik.co>
2025-08-04 02:55:46 +0530

    Enhance: Update Motivational Quotes in Hook
    - Added new motivational quotes in various languages to the QUOTES array.
    - Commented out some existing quotes for better organization and clarity.
    - Improved the diversity of quotes to inspire users more effectively.

--- frontend/src/hooks/useMotivationalQuote.ts
+++ frontend/src/hooks/useMotivationalQuote.ts
@@ -9,10 +9,12 @@ const QUOTES = [
 
   "You will have to hang tough thru this period, but all lies ahead of you, donot stop until you got answers to all question and make your life a success story! - Ashish Vishnoi",
 
+  "Har pehlu zindagi ka intemn hota h, muskilo se bachna aasan hota h, Dar kr bhadne walo ko kuch nhi milta zindagi me, Ladne walo k kadmo m jagah hota h. -- some where in hostel room(jee)",
+
   
   "It’s not that I’m so smart, it’s just that I stay with problems longer. – Albert Einstein",
   // "In the middle of difficulty lies opportunity. – Albert Einstein",
-  "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world. – Albert Einstein",
+  // "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world. – Albert Einstein",
   // "A person who never made a mistake never tried anything new. – Albert Einstein",
   // "The only source of knowledge is experience. – Albert Einstein",
   // "Try not to become a man of success, but rather try to become a man of value. – Albert Einstein",
@@ -21,30 +23,23 @@ const QUOTES = [
   // "Education is what remains after one has forgotten what one has learned in school. – Albert Einstein",
   // "Only those who attempt the absurd can achieve the impossible. – Albert Einstein",
   // "You never fail until you stop trying. – Albert Einstein",
-  "The important thing is not to stop questioning. Curiosity has its own reason for existing. – Albert Einstein",
+  // "The important thing is not to stop questioning. Curiosity has its own reason for existing. – Albert Einstein",
   "I have no special talent. I am only passionately curious. – Albert Einstein",
 
   // Thomas Edison
   // "Opportunity is missed by most people because it is dressed in overalls and looks like work. – Thomas Edison",
   // "I have not failed. I've just found 10,000 ways that won't work. – Thomas Edison",
   "There is no substitute for hard work. – Thomas Edison",
+  "Hard work beats Talent. - xyz",
 
   // Bruce Lee
   "I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times. – Bruce Lee",
   "The successful warrior is the average man, with laser-like focus. – Bruce Lee",
-  // "Knowing is not enough, we must apply. Willing is not enough, we must do. – Bruce Lee",
-
-  // Jocko Willink
-  "Don't expect to be motivated every day to get out there and make things happen. You won't be. Don't count on motivation. Count on discipline. – Jocko Willink",
-
-
-  // Seneca
-  // "Luck is what happens when preparation meets opportunity. – Seneca",
-  // "It is not that we have a short time to live, but that we waste a lot of it. – Seneca",
-
+  
   // Others
   "Work like hell. I mean you just have to put in 80 to 100 hour weeks every week. – Elon Musk",
-  "Success isn't always about greatness. It's about consistency. Consistent hard work gains success. Greatness will come. – Dwayne Johnson",
+  "Working 16 hrs a day, 7 Days a week, 52 Weeks in the year and People still call me lucky",
+
 ];
 
 const STORAGE_KEY = 'motivationalQuote';
